22db8164b7dcc41e3868c7ce1804bdc671925fbf
Jitske <80577121+happygeekme@users.noreply.github.com>
2021-04-20 14:52:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let person = {
+    name: "Jitske",
+    age: 49
+};
+
+//console.log(person)
+
+//console.log(person.name)
+
+//console.log(person.age)
+
+//person["name"] = "Truus";
+
+person["age"] = 25
+
+console.log(person.age)
+
+// let evaluations = [7, 10, 9]
+let evaluations = {
+    cijfer1: 7,
+    cijfer2: 10,
+    cijfer3: 9
+};
+
+console.log(evaluations)
+
+let lege_array = ["groen", "blauw", "rood"]
+
+//  console.log(lege_array)
+// console.log(lege_array[0])
+
+console.log(lege_array.length)
+
+lastItem = lege_array[lege_array.length - 1]
+
+// console.log(lastItem)
+lege_array.push("geel")
+lege_array.push(5)
+lege_array.push(
+    {greeting:"Hi ik ben een object"}
+)
+
+// console.log(lege_array)
+
+console.log(lege_array[lege_array.length -1])
